f04ac003f97b8ba8346a17130bbecf005dc0e8c3
Mikhail Nikorkin <nikorkin1990@mail.ru>
2023-03-02 19:00:39 +0300

    hw3

---
+++ src/main/java/org/example/Task3/Books.java
@@ -0,0 +1,19 @@
+package src.main.java.org.example.Task3;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Books {
+
+    private String name;
+    private String author;
+    private Double price;
+    private Integer year;
+    private Integer pgNum;
+
+
+
+}
+
